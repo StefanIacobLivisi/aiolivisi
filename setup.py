4badfac9c989024b099899e716958b6ef1d8ca63
stefan.iacob <stefan.iacob.extern@livisi.de>
2023-02-24 14:53:40 +0200

    Increment the version to 0.0.16

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ MIN_PYTHON_VERSION = "3.8"
 
 setup(
     name="aiolivisi",
-    version="0.0.15",
+    version="0.0.16",
     license="Apache License 2.0",
     author="Stefan Iacob",
     author_email="stefan.iacob.extern@livisi.de",
